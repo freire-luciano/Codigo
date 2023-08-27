923e0aa0bec9855084e3d81751704bfca8486dea
Luciano Freire <freire.luciano@gmail.com>
2023-08-27 10:49:57 -0300

    POO - Facens 2023/02 - Exercícios

---
+++ PrimeiroPrograma/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args)  {
+        //instrucoes
+        String nome; 
+        nome = "Luciano"; 
+
+        System.out.println("Soma de 2 + 2=" + (2 + 2));
+        System.out.printf("Ola %s %d", nome, 10);
+        System.out.println(10);
+        System.out.println(4.7); 
+        System.out.println('a'); 
+    }
+}
