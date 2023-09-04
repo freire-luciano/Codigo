28a905d8a4e38487bab58188eb92b56e7ecff3d8
Luciano Freire <freire.luciano@gmail.com>
2023-09-04 09:51:02 -0300

    04/09/2023 Manipulação entrada dados

---
+++ TesteStringsExercicios/src/App.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String dadosEntrada; 
+        Scanner entrada = new Scanner(System.in);
+       
+        int dado = 0; 
+        double dadoReal = 0 ; 
+
+       // System.out.println("Digite um texto: ");
+        //dadosEntrada = entrada.nextLine();
+
+        //System.out.println("Texto lido: ");
+        //System.out.println(dadosEntrada);
+
+
+        System.out.println("Digite outro texto para separar dados por espacos");
+         Scanner linhaLida = new Scanner(entrada.nextLine()); 
+        while (linhaLida.hasNext()) {
+            System.out.println(linhaLida.next());
+        }
+
+        System.out.println("Digite outro texto para separar dados por espacos e converter inteiro");
+        linhaLida = new Scanner(entrada.nextLine());
+        while (linhaLida.hasNext()) {
+            dado = Integer.parseInt(linhaLida.next()); 
+            System.out.println("dado convertido = " + dado) ;
+        }
+
+        System.out.println("Digite outro texto para separar dados por espacos e converter Double");
+        linhaLida = new Scanner(entrada.nextLine());
+        while (linhaLida.hasNext()) {
+            dadoReal = Double.parseDouble(linhaLida.next()); 
+            System.out.println("dado convertido = " + dado) ;
+            
+        }
+    }
+}
