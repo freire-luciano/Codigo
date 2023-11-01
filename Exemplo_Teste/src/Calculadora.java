e667490eaa1c2d6b8c236ae5ca2dd482946c5dab
Luciano Freire <freire.luciano@gmail.com>
2023-10-31 22:35:22 -0300

    Aula 31-10-2023

---
+++ Exemplo_Teste/src/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+
+  
+    public double soma (double a, double b) { 
+        return (a+b); 
+    }
+
+
+}
---
+++ Exemplo_Teste/src/CalculadoraTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+    @Test
+    public void testSoma() {
+        Calculadora c1 = new Calculadora(); 
+        assertEquals(4.0, c1.soma(2,2), 0.1); 
+    }
+}
